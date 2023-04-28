a75b9276a1da879737684d5daa9f6570a9040825
juhyang <juhyang22@handong.ac.kr>
2023-04-28 11:16:44 +0900

    온보딩1 페이지

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan')
 // 사용할 파일들 지정
 let loadingRouter = require('./routes/loading.js')
 let postRouter = require('./routes/post.js')
+let onbording1Router = require('./routes/onboarding1.js')
 
 var app = express()
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 //지정파일들 접근경로 설정
 app.use('/', loadingRouter)
 app.use('/p', postRouter)
+app.use('/onboarding1', onboarding1Router)
 
 // error handler
 

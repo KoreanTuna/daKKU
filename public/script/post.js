ea3ebe91a44ccf5dfac1a375bbe28a50b5bd9c30
minwoo <59003468+KoreanTuna@users.noreply.github.com>
2023-05-03 21:44:12 +0900

    Post Editor 기능 생성 / 모달

---
+++ public/script/post.js
@@ -0,0 +1,28 @@
+let tags = [];
+function addTag() {
+    console.log("hi");;
+    const hashTagsEl = document.getElementById('hashTags');
+    const tagListEl = document.getElementById('tagList');
+    const addTagBtn = document.getElementById('addTagBtn');
+    let tag = hashTagsEl.value;
+    // const tag = document.getElementById('hashTags').value;
+    if (tag !== '') {
+        tags.push(tag);
+        // tagListEl.innerText = '';
+        tags.forEach((t) => {
+            const tagEl = document.createElement('div');
+            tagEl.className = 'tag';
+            const tagTextEl = document.createElement('span');
+            tagTextEl.textContent = `#${t}`;
+            tagEl.appendChild(tagTextEl);
+            tagEl.addEventListener('click', () => {
+                tags = tags.filter((item) => item !== t);
+                tagListEl.removeChild(tagEl);
+            });
+            tagListEl.appendChild(tagEl);
+        });
+        hashTagsEl.value = '';
+        tags = [];
+    }
+}
+        

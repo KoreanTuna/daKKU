a44d9a28941956e2e6a76a5a9ae7ababb620fe83
장빛나 <2224com@naver.com>
2023-06-03 20:40:15 +0900

    장빛나

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,9 +1,30 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const db = getFirestore();
+const collectionPost = db.collection('post');
 
 /* GET homepage. */
-router.get('/', function (req, res, next) {
-  res.render('homepage.ejs', {})
+router.get('/', async function (req, res, next) {
+
+  const documentAll = await collectionPost.get();
+  const documentList = documentAll.docs.map(item => item.id);
+  let data= [];
+
+  for(let i = 0; i<documentList.length; i++){
+    let document = collectionPost.doc(documentList[i]);
+    let temp = await document.get();
+    data.push(temp.data());
+  }
+  for(let i = 0; i<data.length; i++){
+    let content = data[i].content.ops;
+    let title = data[i].title;
+    
+    //console.log(content);
+    //console.log(title);
+  }
+  console.log(data);
+  res.render('homepage.ejs', {data:data})
 })
 
 module.exports = router

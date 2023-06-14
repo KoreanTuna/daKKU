e1223c936973b3ec918ca38b7a8d13e2ff7c31da
장빛나 <2224com@naver.com>
2023-06-14 21:39:09 +0900

    커스터마이징 모달 이식하기

--- routes/homepage.js
+++ routes/homepage.js
@@ -38,5 +38,31 @@ router.get('/', async function (req, res, next) {
 
 })
 
+router.post("/saveCustom", (req, res) => {
+  // console.log("save_custom");
+  // console.log(req.body.backgroundURL);
+  // console.log(req.body.folderURL);
+  // console.log(req.body.folderAddURL);
+  const data = {
+    // backg: req.body.title,
+    backgroundURL : req.body.backgroundURL,
+    folderURL : req.body.folderURL,
+    folderAddURL :req.body.folderAddURL,
+    color: req.body.color,
+
+  };
+  //console.log("123123");
+  collectionCustom.doc("custom").set(data)
+  .then(() => {
+    res.status(200).send("Data saved successfully.");
+  })
+  .catch((error) => {
+    console.error("Error saving data:", error);
+    res.status(500).send("Error saving data.");
+  });
+  
+  //console.log("s456456ave_custom");
+});
+
 
 module.exports = router
